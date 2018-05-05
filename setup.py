8546c1dca33958da2f681b195074a52af399d4ef
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-05-05 22:55:12 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name = 'attendees-analyzer',
+    packages = find_packages(),
+    version = '0.0.1',
+    description = 'To analyze attendee data',
+    author = 'Taihsiang Ho (tai271828)',
+    author_email = 'tai271828@gmail.com',
+    url = 'https://github.com/tai271828/attendees-analyzer',
+    download_url = 'https://github.com/tai271828/attendees-analyzer',
+    keywords = ['attendee', 'pycontw'],
+    entry_points={
+        'console_scripts': [
+            'atta=atta.controller.atta_cli:main',
+        ]
+    },
+    classifiers = [
+        "Programming Language :: Python",
+    ]
+)
