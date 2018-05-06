9ea11eba03f2aac68ae2675f1bfc742771ad95c2
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-05-06 23:48:57 +0800

    Add extract-column.py

---
+++ scripts/extract-column.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import csv
+
+output = []
+
+with open('../../attendees-analyzer-working/2017Attendees.csv', 'r') as csvfile:
+    spamreader = csv.reader(csvfile)
+    for row in spamreader:
+        output.append(row[12])
+
+print('Now saving...')
+
+with open('../../attendees-analyzer-working/titles.csv', 'w') as csvfile:
+    spamwriter = csv.writer(csvfile)
+    for row in output:
+        spamwriter.writerow([row])
+
