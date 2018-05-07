4f93614b9da3f3bb99f61d10d87bd95f3c8491d4
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-05-08 01:18:12 +0800

    Revert "Refine regex. Combine categories professor and researcher"
    This reverts commit b78eb6eccd83b22f843cffe2a059690708a2f723.

--- atta/cleaner/title.py
+++ atta/cleaner/title.py
@@ -12,20 +12,21 @@ def cat_title(title):
     """
     title = str(title)
     pattern_engineer = "(?i)[engineer]{6,}|engr|develop|code|software|工程師|碼|程式|資訊|program|軟體|設計|專員|IT|" \
-                       "Analysts|SW|AP|PG|F2E|DevOps|architect|^R.*D$|開發|bug|hacker"
+                       "Analysts|SW|AP|PG|F2E|DevOps|architect|^R.*D$|開發"
     pattern_student = "學生|(?i)student"
-    pattern_academia = "(?i)phd|博|postdoc|research|研究|PI|professor|教授"
+    pattern_research = "(?i)phd|博|postdoc|research|研究"
     pattern_job_seek = "(?i)待業|Home|job|自由業|助理[^教授]|Freelance|self-employed|無|0|沒有人|nobody|nan|自由業|none"
     # 助理 could be a temporary job therefore potential job seeker, but not 助理教授
     pattern_head = "(?i)C.O|chief|lead|chair|director|長|總|founder"
+    pattern_professor = "(?i)PI|professor|教授"
     pattern_data_scientist = "(?i)data|資料|使用|經驗|分析|scientist"
     pattern_consultant = "(?i)consultant|顧問"
     pattern_manager = "(?i)manager|[^助]理|pm"
     pattern_graphics = "(?i)graph|art|cgi"
     pattern_dic = {'Engineer': pattern_engineer, 'Consultant': pattern_consultant,
                    'Data Scientist': pattern_data_scientist,
-                   'Potential Job Seeker': pattern_job_seek, 'Head': pattern_head,
-                   'Manager': pattern_manager, 'Graphics': pattern_graphics, 'Academia': pattern_academia,
+                   'Potential Job Seeker': pattern_job_seek, 'Head': pattern_head, 'Professor': pattern_professor,
+                   'Manager': pattern_manager, 'Graphics': pattern_graphics, 'Researcher': pattern_research,
                    'Student': pattern_student}
     title_cat = ''
     for pattern in pattern_dic:
