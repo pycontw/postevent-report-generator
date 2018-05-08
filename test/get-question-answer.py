10b1afcba58c4cc33e550942238f59b1dff4d92c
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-05-09 00:15:50 +0800

    Update the title unittest data and helper scripts

--- test/get-question-answer.py
+++ test/get-question-answer.py
@@ -2,7 +2,7 @@
 from title import cat_title
 
 
-with open('test-db.txt') as db:
+with open('../../attendees-analyzer-working/titles.csv') as db:
     lines = db.readlines()
     for line in lines:
         line = line.rstrip()
