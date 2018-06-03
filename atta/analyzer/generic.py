fab0c4fe4f46864562c4ea6e2247ccedde0f26cd
zztin <32836585+zztin@users.noreply.github.com>
2018-06-03 23:52:45 +0200

    Generic analysis for csv data with user help to select columns.

--- atta/analyzer/generic.py
+++ atta/analyzer/generic.py
@@ -5,202 +5,107 @@ import matplotlib.pyplot as plt
 import re
 import logging
 import os
-from atta.cleaner.title import cat_title
+import collections
+# from atta.cleaner.title import cat_title
 
+def cat_title(title):
+    """
+    Return the category of a given title.
 
-def go(readcsv):
+    >>> cat_title("工程師")
+    'Engineer'
 
-    '''replace hardcoded line with CLI input'''
-    #df = pd.read_csv("../attendees-analyzer-working/2017Attendees.csv")
-    df = pd.read_csv(os.getcwd() + "/" + readcsv)
+    """
+    title = str(title)
+    pattern_dic = collections.OrderedDict()
+    pattern_dic['Potential Job Seeker'] = "(?i)待業|Home|job|自由業|助理[^教授]|" \
+                                          "Freelance|self-employed|無|0|沒有人|nobody|nan|自由業|none"
+    # 助理 could be a temporary job therefore potential job seeker, but not 助理教授
+    pattern_dic['Head'] = "(?i)C.O|chief|lead|chair|director|長|總|founder"
+    pattern_dic['Manager'] = "(?i)manager|[^助]理|pm"
+    pattern_dic['Engineer'] = "(?i)[engineer]{6,}|engr|develop|code|software|工程師|碼|程式|資訊|program|" \
+                              "軟體|設計|IT|Analysts|SW|AP|PG|F2E|DevOps|architect|^R.*D$|開發|bug|hacker"
+    #'設計'有點不精確...
+    pattern_dic['Student'] = "學生|(?i)student"
+    pattern_dic['Academia'] = "(?i)phd|博|postdoc|research|研究|PI|professor|教授"
+    pattern_dic['Data Scientist'] = "(?i)data|資料|使用|經驗|分析|scientist"
+    pattern_dic['Consultant'] =  "(?i)consultant|顧問"
 
-
-    # paid date could be relating to "information acquired " earlier vs later
-    logging.debug(df.keys())
-
-    df.rename(index=str, columns={"Id": "ID", "Gender / 性別": "Gender","Nationality / 國籍": "Nationality","服務公司(學生或老師請填學校名稱+系所) / Company(Students or teachers please fill in the school name + Department)":"Organization","職稱(如果身分是學生請填”學生”) / Job Titles(If you are a student please fill in \"student\")":"Title","Diet / 飲食":"Special food requirments","Size of T-shirt / T恤尺寸":"Size","發票抬頭(購買 \"個人/企業一般票\" 才需要填寫) / Invoiced Company Name":"Invoice Company name"}, inplace = True)
-
-
-    df.drop("# invoice policy #",axis = 1, inplace = True)
-
-    # Plotting categorical
-    plt.subplots(figsize=(18,5))
-    sns.countplot(x='Gender',data=df)
-
-    fig, ax = plt.subplots()
-    fig.set_size_inches(11.7, 8.27)
-    sns.countplot(x='Nationality',data=df)
-
-
-    # ## Create title category list and plot
-
-    organ_list = df['Organization'].unique().tolist()
-    title_list = df['Title'].unique().tolist()
-    # Categories
-    # Find ones without jobs
-    organ_count = len(organ_list)
-    title_count = len(title_list)
-    print(organ_count, title_count)
-
-
-    nojob = df.loc[df['Title'] == np.nan]
-
-    title_count = df['Title'].value_counts()
-    title_count = title_count.to_frame(name = "Count").reset_index()
-    title_count.columns.values[0] = 'Title'
-    title_count
-
-    title_count[title_count['Count'] > 4]
-
-
-    title_list
-
-
-    # Create main categories in a new column.
-
-    #?? what is PA? how to categorize analyser?
-
-    pattern_engineer = "(?i)[engineer]{6,}|engr|developer|code|software|工程師|碼|程式|資訊|program|軟體|設計|專員|IT|Analysts" # 專員?
-    pattern_developer = "(?i)develop|architect|^R.*D$|開發"
-    pattern_student = "學生|(?i)student"
-    pattern_nobody = "(?i)無|0|沒有人|nobody|nan|自由業|none|self-employed"
-    # why no* match everyting???
-    pattern_research = "(?i)phd|博|postdoc|research|研究"
-    pattern_job_seek = "(?i)待業|Home|job|自由業|助理[^教授]|Freelance|self-employed"
-    # 助理 could be a temporary job, but not 助理教授
-    pattern_head = "(?i)C.O|chief|lead|chair|director|長|總|founder"
-    pattern_professor = "(?i)PI|professor|教授"
-    pattern_data_scientist = "(?i)data|資料|使用|經驗|分析|scientist"
-    pattern_consultant = "(?i)consultant|顧問"
-    pattern_manager = "(?i)manager|[^助]理|pm"
-    pattern_graphics = "(?i)graph|art|cgi"
-    title_categories = {"graphic_artist":[],"developer":[],"uncategorized":[],
-                        "consultant":[],"data_scientist":[],"job_seeker":[],
-                        "engineer":[],'student':[],'professor':[],'researcher':[],
-                        'nobody':[],'head':[],'manager':[]}
-
-    # Assign to categories
-    # Find participants without jobs
-
-    for title in title_list:
-        try:
-            if re.search(pattern_head, title) != None:
-                title_categories['head'].append(title)
-            elif re.search(pattern_manager, title) != None:
-                title_categories['manager'].append(title)
-            elif re.search(pattern_developer, title) != None:
-                title_categories['developer'].append(title)
-            elif re.search(pattern_engineer, title) != None:
-                title_categories['engineer'].append(title)
-            elif re.search(pattern_data_scientist, title) != None:
-                title_categories['data_scientist'].append(title)
-            elif re.search(pattern_student, title) != None:
-                title_categories['student'].append(title)
-            elif re.search(pattern_nobody, title) != None:
-                title_categories['nobody'].append(title)
-            elif re.search(pattern_job_seek, title) != None:
-                title_categories['job_seeker'].append(title)
-            elif re.search(pattern_professor, title) != None:
-                title_categories['professor'].append(title)
-            elif re.search(pattern_consultant, title) != None:
-                title_categories['consultant'].append(title)
-            elif re.search(pattern_research, title) != None:
-                title_categories['researcher'].append(title)
-            elif re.search(pattern_graphics, title) != None:
-                title_categories['graphic_artist'].append(title)
-            else:
-                title_categories['uncategorized'].append(title)
-        except TypeError:
+    title_cat = ''
+    for pattern in pattern_dic:
+        if re.search(pattern_dic[pattern], title) is not None:
+            title_cat = pattern
+        else:
             pass
+    return title_cat
 
+def welcome_ask_year():
+    print("Welcome to the PyCon attendies analyzer.")
+    year = int(input('Please enter the year to be analyzed: '))
+    return year
 
-    key = []
-    counts = []
-    j = -1
-    for i in title_categories.values():
-        j += 1
-        key.append(list(title_categories.keys())[j])
-        counts.append(len(i))
+def select_column(df):
+    cols = df.keys().tolist()
+    for i, col in enumerate(cols):
+        print(i,':',col)
+    registration_date = int(input('Please select the number for "報名日期/Paid date": '))
+    nationality = int(input('Please select the number for "國籍/Nationality": '))
+    gender = int(input('Please select the number for "性別/Gender": '))
+    organization = int(input('Please select the number for "服務單位/Company": '))
+    job_title = int(input('Please select the number for "職稱/Job Titles": '))
 
-    title_cat_counts = pd.DataFrame({'1_Title':key,'2_Counts':counts})
-    title_cat_counts['3_Name'] = list(title_categories.values())
-    title_cat_counts
-    title_cat_counts.sort_values('2_Counts',ascending=False, inplace= True)
-    title_cat_counts.reset_index(drop = True)
+    # col_index = {'job_title':job_title,'organization':organization,'gender':gender,
+    #              'registration_date':registration_date,'nationality':nationality}
+    df_selected = df.iloc[:,[gender, nationality, registration_date, job_title]]
+    df_selected.columns = ['Gender','Nationality','Registration_date','Title']
+    return df_selected
 
-    title_cat_counts.iloc[1]['3_Name']
-
-
-
-
-
-
-
-
-
-    new_col = np.nan
-    #df.insert(loc=12, column='Title_Categories', value=new_col)
+def add_cat_title(df):
     df['Title_Categories'] = df['Title'].apply(cat_title)
-    df
-
-
-    # In[32]:
-
-
-    pattern_nobody = "(?i)無|0|沒有人|nobody|nan|自由業|none|self-employed"
-
-    pattern = '家|自'
-
-    # df2 =pd.DataFrame( {'A':[1,2,3],
-    #      'B':[4,5,6],'C':[7,8,9]})
-    # df3 = df2[['A','B']]
-    # df3
-
-    # Without a job: See from Organization
-
-    # Nan: 32,71,101,104,325,569
-    # none: 194, 274, 276,301,375,426, 498
-    # 不告訴你： 9,263,560
-    # 家裡個人接案：435,436,465,
-    # 560 : 表情符號...
-
-    # 488是駭客！
-
-    df[['Organization','Title']].iloc[9]
-
-
-    # ## Plot title categories
+#    title_category = df.columns.get_loc("Title_Categories")
+    return df
 
 
 
-    df['Title_Categories'].value_counts('Job Seeker')
+def plot_counts(df,year):
+    cols = df.keys().tolist()
+    bar_cols = [cols[0],cols[1],cols[4]]  # change here to include more columns for count plots
+    for col in bar_cols:
+        fig, ax = plt.subplots(figsize=(12, 8))
+        plt.title(str(col)+' of the Attendees at PyCon Taiwan in ' + str(year))
+        ax.set_xticklabels(str(col), rotation=0, fontdict={"fontsize": '8'})
+        ax.set_xlabel(xlabel=str(col))
+        ax.set_ylabel(ylabel="Counts")
+        sns.set(font_scale=2)
+        sns.countplot(x = str(col), data = df)
+        # directory = Tk.tk() # Something about
+        plt.savefig(str(col) + str(year) + '.jpg')
+
+# def plot_accumulated_count(df,year):
+
+#
+
+# def save_df():
+#     df.to_pickle(str(year)+"_analyzed_data")  ## need to refine directory
 
 
+def go(csvfile):
 
-
-    fig, ax = plt.subplots()
-    fig.set_size_inches(11.7, 8.27)
-    ax.set_xticklabels('1_Title',rotation=90,fontdict= {"fontsize": '16'})
-    ax.set_xlabel(xlabel="Job Title")
-    ax.set_ylabel(ylabel= "Counts")
-    sns.set(font_scale = 2)
-
-    sns.countplot(x = 'Title_Categories', data = df, ax = ax,
-                  order = ['Engineer', 'Student', 'Developer', 'Manager', 'Researcher',
-           'Data Scientist', 'Head', 'Professor', 'Job Seeker', 'Consultant',
-           'Graphics', 'Others'])
-
-
-    fig, ax = plt.subplots()
-    fig.set_size_inches(11.7, 8.27)
-    ax.set_xticklabels('1_Title',rotation=45,fontdict= {"fontsize": '16'})
-    ax.set_xlabel(xlabel="Job Title")
-    ax.set_ylabel(ylabel= "Counts")
-    sns.set(font_scale = 2)
-
-    sns.barplot(x = '1_Title', y = "2_Counts", data = title_cat_counts, ax = ax)
-
-    plt.show()
+    '''Get file. Replace hardcoded line with CLI input'''
+    # welcome user, ask year of data
+    year = welcome_ask_year()
+    # read csv as pandas dataframe
+    df = pd.read_csv(os.getcwd() + "/" + csvfile)
+    # Print the keys as bug messages
+    logging.debug(df.keys())
+    # select necessary columns from df
+    df = select_column(df)
+    df = add_cat_title(df)
+    plot_counts(df,year)
+    print('Go process finished completely. Find results in the same directory as the csv file.')
 
 def fake_go():
     print('fake_go is executed.')
+
+
+go('2017attendees.csv')
