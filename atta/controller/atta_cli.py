c4adad40c17639d9d1665be289b5b148f26d28d3
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-06-13 22:28:00 +0800

    PEP8 fix

--- atta/controller/atta_cli.py
+++ atta/controller/atta_cli.py
@@ -1,11 +1,13 @@
 import atta.analyzer.generic as ag
 import click
 
+
 @click.command()
 @click.option('--readcsv', default="data.csv", help='read csv format data')
 def main(readcsv):
     print("Lets analyze something.")
     ag.go(readcsv)
+
 
 if __name__ == '__main__':
     main()
