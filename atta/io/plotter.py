dd46e3f4cff519261d525bd57f0ff59853ab8d32
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-06-18 12:12:17 +0800

    Encapsulating plot_count

--- atta/io/plotter.py
+++ atta/io/plotter.py
@@ -7,12 +7,16 @@ def plot_counts(df, year):
     # change here to include more columns for count plots
     bar_cols = [cols[0], cols[1], cols[4]]
     for col in bar_cols:
-        fig, ax = plt.subplots(figsize=(12, 8))
-        plt.title(str(col)+' of the Attendees at PyCon Taiwan in ' + str(year))
-        ax.set_xticklabels(str(col), rotation=0, fontdict={"fontsize": '8'})
-        ax.set_xlabel(xlabel=str(col))
-        ax.set_ylabel(ylabel="Counts")
-        sns.set(font_scale=2)
-        sns.countplot(x = str(col), data = df)
-        # directory = Tk.tk() # Something about
-        plt.savefig(str(col) + str(year) + '.jpg')
+        plot_count(df, col, year)
+
+
+def plot_count(df, col, year):
+    fig, ax = plt.subplots(figsize=(12, 8))
+    plt.title(str(col)+' of the Attendees at PyCon Taiwan in ' + str(year))
+    ax.set_xticklabels(str(col), rotation=0, fontdict={"fontsize": '8'})
+    ax.set_xlabel(xlabel=str(col))
+    ax.set_ylabel(ylabel="Counts")
+    sns.set(font_scale=2)
+    sns.countplot(x = str(col), data = df)
+    # directory = Tk.tk() # Something about
+    plt.savefig(str(col) + str(year) + '.jpg')
