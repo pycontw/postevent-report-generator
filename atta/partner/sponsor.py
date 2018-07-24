da1b11271ff4f92b0a8c2db199c3e8a7837a5741
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-07-24 17:22:46 +0800

    Try factory pattern for sponsors

--- atta/partner/sponsor.py
+++ atta/partner/sponsor.py
@@ -1,7 +1,7 @@
 import logging
 import pkg_resources
 import atta.io.yaml as attayaml
-
+from abc import ABCMeta, abstractmethod
 
 logger = logging.getLogger('atta')
 
@@ -19,12 +19,31 @@ yaml_packages = attayaml.read_yaml(template_packages.name)
 yaml_sponsors = attayaml.read_yaml(template_sponsors.name)
 
 
-class Package:
+class Package(metaclass=ABCMeta):
 
-    def __init__(self):
+    def __init__(self, package_name, sponsor_name):
+        self.package_name = package_name
+        self.package_content_flag = yaml_packages[package_name]
+        self.package_content_generic_flag = yaml_packages['generic']
+        self.sections = []
+        self.create_profile(package_name, sponsor_name)
+
+    @abstractmethod
+    def create_profile(self, name):
         pass
+
+    def get_sections(self):
+        return self.sections
+
+    def add_section(self, section):
+        self.sections.append(section)
+
+    @property
+    def description(self):
+        pass
+
 
 class Sponsor(Package):
 
-    def get_sponsors(self):
+    def create_profile(self, package_name, sponsor_name):
         pass
