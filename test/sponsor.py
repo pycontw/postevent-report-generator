2a42e16987420009286af111b3d09b8b1e9a9c2f
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-07-25 18:16:25 +0800

    More sponsor test cases

--- test/sponsor.py
+++ test/sponsor.py
@@ -4,11 +4,20 @@ from atta.partner import sponsor
 
 
 class TestSponsor(unittest.TestCase):
-
-    def test_attributes(self):
+    def setUp(self):
         sponsors = sponsor.get_all_sponsors('./data/packages.yaml',
                                             './data/sponsors.yaml')
-        self.assertEqual(len(sponsors), 2)
+        self.sponsors = sponsors
+
+    def test_sponsor_number(self):
+        self.assertEqual(len(self.sponsors), 2)
+
+    def test_sponsor_attributes(self):
+        for sponsor in self.sponsors:
+            if sponsor.name == 'Gandi':
+                target_sponsor = sponsor
+
+        self.assertEqual(target_sponsor.name, 'Gandi')
 
 
 if __name__ == '__main__':
