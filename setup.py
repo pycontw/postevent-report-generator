72228a1bfb9e9af785466406b217eda7f05173a4
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2019-12-15 16:17:24 +0800

    Bump version to update license and long description

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name="pycontw-report-generator",
     packages=["report_generator"],
     zip_safe=False,
     include_package_data=True,
-    version="1.0.0",
+    version="1.0.1",
     description="PyCon TW post-event report generator.A cli command to generate PyCon TW post-event reports. Previously known as attendee-analyzer",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Taihsiang Ho (tai271828)",
     author_email="tai271828@gmail.com",
+    license='BSD 3-Clause',
     url="https://github.com/pycontw/pycontw-postevent-report-generator",
     download_url="https://github.com/pycontw/pycontw-postevent-report-generator.git",
     keywords=["attendee", "pycontw", "post-event", "report-generator"],
