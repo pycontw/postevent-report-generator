6dbe9e2c263192fdb4453ba1d79b723c0b5b9163
Wei Lee <weilee.rx@gmail.com>
2020-03-07 08:59:38 +0800

    build(Pipfile): add pre-commit to dev package

--- tasks/env.py
+++ tasks/env.py
@@ -17,3 +17,12 @@ def init(ctx):
 def init_dev(ctx):
     """Install development dependencies"""
     ctx.run("pipenv install --dev")
+
+
+@task
+def setup_pre_commit_hook(ctx):
+    """Setup pre-commit hook to automate check before git commit and git push"""
+    ctx.run(
+        "pipenv run pre-commit install -t pre-commit \\"
+        "pipenv run pre-commit install -t pre-push"
+    )
