71b010ccad2ff9f503bec3669fefd4b9968bceee
Wei Lee <weilee.rx@gmail.com>
2020-04-03 18:17:06 +0800

    fix(setup): fix documentaion path in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 from setuptools import find_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+with open(path.join(this_directory, "docs/index.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
